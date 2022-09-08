55045278adf199f8fcdc0ef1bd9f39aa17ec1779
DennisDon678 <102510196+DennisDon678@users.noreply.github.com>
2022-09-08 19:52:35 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- *
- *main - prints a given string
- *
- */
+*main - prints a given string
+*
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
