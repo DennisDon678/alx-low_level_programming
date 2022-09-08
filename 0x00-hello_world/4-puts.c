6b31c571eea438357ffcda13f9069ee8bfff54e4
DennisDon678 <102510196+DennisDon678@users.noreply.github.com>
2022-09-08 13:10:48 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,9 @@
     *
     *Return: Always 0 (success)
     */
-int main (void)
+int main(void)
 {
-  char txt[60] = "\"Programming is like building a multilingual puzzle";
-  puts(txt);
-  return(0);
-    }
+char txt[60] = "\"Programming is like building a multilingual puzzle";
+puts(txt);
+  return (0);
+}
