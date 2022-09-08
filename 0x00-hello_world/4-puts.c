22b0de1bc809a0a4df5e5b8ca4a4bae54f93f40e
Dennis <dennisdon678@gmail.com>
2022-09-08 04:55:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+
+   /**
+    *Main -Entry point 
+    *
+    *Return: Always 0 (success)
+    */
+int main (void)
+{
+  char txt[60] = "\"Programming is like building a multilingual puzzle";
+  puts(txt);
+  return(0);
+    }
